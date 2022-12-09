6ae08baceb1f0be8bec034d218493b4ce3ace53d
Robin Green <robin@designingreen.co.uk>
2022-12-09 14:58:31 +0000

    Update to banner advert

--- template-parts/banner-advert.php
+++ template-parts/banner-advert.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row g-0">
 			<div class="col-12 text-center">
-				<h3 style="margin-bottom: 0;">Banner advert&nbsp;&nbsp;•&nbsp;&nbsp;Banner advert&nbsp;&nbsp;•&nbsp;&nbsp;Banner advert</h3>
+				<h3 class="atm--banneradvert"><?php the_field('banneradvert_content'); ?>&nbsp;<a href="<?php the_field('banneradvert_ctalink'); ?>"><?php the_field('banneradvert_cta'); ?></a></h3>
 			</div>
 		</div>
 	</div>
