90167dacc54f14fa059add714eeaefdc78cfccc2
Robin Green <robin@designingreen.co.uk>
2022-12-14 11:37:25 +0000

    Update to page introduction

--- template-parts/materials-classic-lg.php
+++ template-parts/materials-classic-lg.php
@@ -13,87 +13,88 @@
 		        $my_query = new WP_Query($args);
 		        $total_materials = $my_query->found_posts; ?>
 
-		        	<?php if( $my_query->have_posts() ) { ?>
+	        	<?php if( $my_query->have_posts() ) { ?>
 
-				        <?php if($total_materials == ('6')) { ?>
-			        		<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
-			        			<div class="col-6 col-lg-4">
-					        		<?php if( get_field('mat_contrast') == 'light' ) { ?>
-					        			<a href="<?php the_permalink(); ?>">
-							        		<div class="atm--relative atm--contrast-light">
-							        			<?php get_template_part('template-parts/part-material-lg'); ?>
-							        		</div>
-							        	</a>
-					        		<?php } elseif( get_field('mat_contrast') == 'dark' ) { ?>
-					        			<a href="<?php the_permalink(); ?>">
-						        			<div class="atm--relative atm--contrast-dark">
-						        				<?php get_template_part('template-parts/part-material-lg'); ?>
-							        		</div>
-						        		</a>
-						        	<?php } ?>
-			        			</div>
-			        		<?php endwhile; ?>
-				        <?php } elseif($total_materials == ('7')) { ?>
-				        	<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
-			        			<div class="col-6 col-lg-3">
-					        		<?php if( get_field('mat_contrast') == 'light' ) { ?>
-					        			<a href="<?php the_permalink(); ?>">
-							        		<div class="atm--relative atm--contrast-light">
-							        			<?php get_template_part('template-parts/part-material-lg'); ?>
-							        		</div>
-							        	</a>
-					        		<?php } elseif( get_field('mat_contrast') == 'dark' ) { ?>
-					        			<a href="<?php the_permalink(); ?>">
-						        			<div class="atm--relative atm--contrast-dark">
-						        				<?php get_template_part('template-parts/part-material-lg'); ?>
-							        		</div>
-						        		</a>
-						        	<?php } ?>
-			        			</div>
-				        	<?php endwhile; ?>
-				        	<div class="col-6 col-lg-3">
-				        		<figure>
-				        			<img class="mw-100" src="<?php the_field('additional_material', 'option'); ?>">
-				        		</figure>
-				        	</div>
-				        <?php } elseif($total_materials == ('8')) { ?>
-				        	<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
-			        			<div class="col-6 col-lg-3">
-					        		<?php if( get_field('mat_contrast') == 'light' ) { ?>
-					        			<a href="<?php the_permalink(); ?>">
-							        		<div class="atm--relative atm--contrast-light">
-							        			<?php get_template_part('template-parts/part-material-lg'); ?>
-							        		</div>
-							        	</a>
-					        		<?php } elseif( get_field('mat_contrast') == 'dark' ) { ?>
-					        			<a href="<?php the_permalink(); ?>">
-						        			<div class="atm--relative atm--contrast-dark">
-						        				<?php get_template_part('template-parts/part-material-lg'); ?>
-							        		</div>
-						        		</a>
-						        	<?php } ?>
-			        			</div>
-				        	<?php endwhile; ?>
-				        <?php } elseif($total_materials == ('9')) { ?>
-				        	<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
-				        		<div class="col-lg-4">
-					        		<?php if( get_field('mat_contrast') == 'light' ) { ?>
-					        			<a href="<?php the_permalink(); ?>">
-							        		<div class="atm--relative atm--contrast-light">
-							        			<?php get_template_part('template-parts/part-material-lg'); ?>
-							        		</div>
-							        	</a>
-					        		<?php } elseif( get_field('mat_contrast') == 'dark' ) { ?>
-					        			<a href="<?php the_permalink(); ?>">
-						        			<div class="atm--relative atm--contrast-dark">
-						        				<?php get_template_part('template-parts/part-material-lg'); ?>
-							        		</div>
-						        		</a>
-						        	<?php } ?>
-			        			</div>
-			        		<?php endwhile; ?>
-				        <?php } ?>
-				    <?php } wp_reset_query(); ?>
+			        <?php if($total_materials == ('6')) { ?>
+		        		<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
+		        			<div class="col-6 col-lg-4">
+				        		<?php if( get_field('mat_contrast') == 'light' ) { ?>
+				        			<a href="<?php the_permalink(); ?>">
+						        		<div class="atm--relative atm--contrast-light">
+						        			<?php get_template_part('template-parts/part-material-lg'); ?>
+						        		</div>
+						        	</a>
+				        		<?php } elseif( get_field('mat_contrast') == 'dark' ) { ?>
+				        			<a href="<?php the_permalink(); ?>">
+					        			<div class="atm--relative atm--contrast-dark">
+					        				<?php get_template_part('template-parts/part-material-lg'); ?>
+						        		</div>
+					        		</a>
+					        	<?php } ?>
+		        			</div>
+		        		<?php endwhile; ?>
+			        <?php } elseif($total_materials == ('7')) { ?>
+			        	<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
+		        			<div class="col-6 col-lg-3">
+				        		<?php if( get_field('mat_contrast') == 'light' ) { ?>
+				        			<a href="<?php the_permalink(); ?>">
+						        		<div class="atm--relative atm--contrast-light">
+						        			<?php get_template_part('template-parts/part-material-lg'); ?>
+						        		</div>
+						        	</a>
+				        		<?php } elseif( get_field('mat_contrast') == 'dark' ) { ?>
+				        			<a href="<?php the_permalink(); ?>">
+					        			<div class="atm--relative atm--contrast-dark">
+					        				<?php get_template_part('template-parts/part-material-lg'); ?>
+						        		</div>
+					        		</a>
+					        	<?php } ?>
+		        			</div>
+			        	<?php endwhile; ?>
+			        	<div class="col-6 col-lg-3">
+			        		<figure>
+			        			<img class="mw-100" src="<?php the_field('additional_material', 'option'); ?>">
+			        		</figure>
+			        	</div>
+			        <?php } elseif($total_materials == ('8')) { ?>
+			        	<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
+		        			<div class="col-6 col-lg-3">
+				        		<?php if( get_field('mat_contrast') == 'light' ) { ?>
+				        			<a href="<?php the_permalink(); ?>">
+						        		<div class="atm--relative atm--contrast-light">
+						        			<?php get_template_part('template-parts/part-material-lg'); ?>
+						        		</div>
+						        	</a>
+				        		<?php } elseif( get_field('mat_contrast') == 'dark' ) { ?>
+				        			<a href="<?php the_permalink(); ?>">
+					        			<div class="atm--relative atm--contrast-dark">
+					        				<?php get_template_part('template-parts/part-material-lg'); ?>
+						        		</div>
+					        		</a>
+					        	<?php } ?>
+		        			</div>
+			        	<?php endwhile; ?>
+			        <?php } elseif($total_materials == ('9')) { ?>
+			        	<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
+			        		<div class="col-lg-4">
+				        		<?php if( get_field('mat_contrast') == 'light' ) { ?>
+				        			<a href="<?php the_permalink(); ?>">
+						        		<div class="atm--relative atm--contrast-light">
+						        			<?php get_template_part('template-parts/part-material-lg'); ?>
+						        		</div>
+						        	</a>
+				        		<?php } elseif( get_field('mat_contrast') == 'dark' ) { ?>
+				        			<a href="<?php the_permalink(); ?>">
+					        			<div class="atm--relative atm--contrast-dark">
+					        				<?php get_template_part('template-parts/part-material-lg'); ?>
+						        		</div>
+					        		</a>
+					        	<?php } ?>
+		        			</div>
+		        		<?php endwhile; ?>
+			        <?php } ?>
+			        
+			    <?php } wp_reset_query(); ?>
 		</div>
 	</div>
 </section>
--- template-parts/page-introduction.php
+++ template-parts/page-introduction.php
@@ -1,9 +1,9 @@
 <section class="org--introduction-page ut--block-margin">
 	<div class="container">
 		<div class="row justify-content-center">
-			<div class="col-10">
-				<h1><?php the_field('title_heading') ?></h1>
-				<p><?php the_field('title_introduction') ?></p>
+			<div class="col-12 col-md-10">
+				<h1 class="atm--introduction"><?php the_field('title_heading') ?></h1>
+				<p class="atm--introduction"><?php the_field('title_introduction') ?></p>
 				<?php if (get_field('title_cta')): ?>
 					<div class="atm--button-cta">
 						<a class="button button-cta" href="<?php the_field('title_cta_link'); ?>"><?php the_field('title_cta_command'); ?><i class="far fa-long-arrow-right"></i></a>
